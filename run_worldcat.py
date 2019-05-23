79d0b58f2e7de7e6d1b78c248548361f3fb26172
JoKons <johanna.konstanciak@gmx.de>
2019-05-23 13:22:58 +0200

    inserted get_settings, has dictionary as return value which is used as parameter in the following scripts

--- run_worldcat.py
+++ run_worldcat.py
@@ -6,6 +6,7 @@ import yaml
 import getmetadata 
 import gethtmlworldcat
 import createpublicationtable
+import get_settings
 
 configfile = "config.yaml"
 
@@ -13,11 +14,10 @@ configfile = "config.yaml"
 def main(configfile): 
     with open(configfile, 'r') as configfile:
         config = yaml.load(configfile)
-    #getmetadata.main(config["xmlpath"])
-    #gethtmlworldcat.main(config["plain_suchstring"], 
-    #                     config["csv_file"],
-    #                     config["write_file"])
-    createpublicationtable.main(config["htmlpages"])
+    settings_dict = get_settings.main(config["lang"], config["xmlpath"], config["level"], config["csv_file"], config["write_file"], config["htmlpages"])
+    getmetadata.main(settings_dict)
+    gethtmlworldcat.main(settings_dict, config["plain_suchstring"])
+    createpublicationtable.main(settings_dict)
     
 
 main(configfile)
