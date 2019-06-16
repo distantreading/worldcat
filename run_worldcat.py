638ba86a1e5d4945d72f5ebbbe392b63bddf8059
christofs <c.schoech@gmail.com>
2019-06-16 12:40:47 +0200

    changed naming convention to get_xxx structure

--- run_worldcat.py
+++ run_worldcat.py
@@ -3,10 +3,10 @@
 # Imports and parameters
 
 import yaml 
-import getmetadata 
-import gethtmlworldcat
-import createpublicationtable
 import get_settings
+import get_metadata 
+import get_htmlworldcat
+import create_publicationtable
 import create_summary
 
 configfile = "config.yaml"
@@ -16,9 +16,9 @@ def main(configfile):
     with open(configfile, 'r') as configfile:
         config = yaml.safe_load(configfile)
     settings_dict = get_settings.main(config["lang"], config["xmlpath"], config["level"], config["csv_file"], config["write_file"], config["htmlpages"])
-    getmetadata.main(settings_dict)
-    gethtmlworldcat.main(settings_dict, config["plain_suchstring"])
-    createpublicationtable.main(settings_dict)
+    get_metadata.main(settings_dict)
+    get_htmlworldcat.main(settings_dict, config["plain_suchstring"])
+    create_publicationtable.main(settings_dict)
     create_summary.main(settings_dict)
     
 
