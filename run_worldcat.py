531a0410b736dfc55835c3af8359b044f6431887
JoKons <johanna.konstanciak@gmx.de>
2019-06-25 14:06:03 +0200

    changed parameter in get_htmlworldcat.main

--- run_worldcat.py
+++ run_worldcat.py
@@ -17,7 +17,7 @@ def main(configfile):
         config = yaml.safe_load(configfile)
         settings_dict = get_settings.main(config["lang"], config["basedir"], config["level"], config["write_file"], config["htmlpages"])
         get_metadata.main(settings_dict)
-        get_htmlworldcat.main(settings_dict, config["plain_suchstring"])
+        get_htmlworldcat.main(settings_dict)
         create_publicationtable.main(settings_dict)
         create_summary.main(settings_dict)
     
