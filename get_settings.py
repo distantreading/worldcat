376256f20fa405accf8998827d6ff2f9e135757f
JoKons <44778082+JoKons@users.noreply.github.com>
2020-07-13 12:46:51 +0200

    changed paths to be more generic

--- get_settings.py
+++ get_settings.py
@@ -43,19 +43,21 @@ def match_langs(lang, d, chosen_dict, chosen_key):
         
     return d
 
-def get_write_file(d, write_file): # for creating or using a sub-file for each language, where html-pages will be stored
+def get_write_file(d, wdir): # for creating or using a sub-file for each language, where html-pages will be stored
     
-    new_write_file = join(write_file, d["lang"])
+    new_write_file = join(wdir, "html", d["lang"])
+    #new_write_file = join(write_file, d["lang"])
     #print("new write file", new_write_file)
     
     d["write_file"] = new_write_file
     #print(new_write_file)
     return new_write_file, d
 
-def get_html_file(d, htmlpages): # will be used to get the right html pages, based on the htmlpages-parameter of the config.yaml
+def get_html_file(d, wdir): # will be used to get the right html pages, based on the htmlpages-parameter of the config.yaml
     
-    html_folder, ext = htmlpages.split("/")
-    html_folder = join(html_folder, d["lang"], ext)
+    html_folder = join(wdir, "html", d["lang"], "*.html")
+    #html_folder, ext = htmlpages.split("/")
+    #html_folder = join(html_folder, d["lang"], ext)
     #print("html_folder", html_folder)
     #print("ext", ext)
     d["html_folder"] = html_folder
@@ -82,8 +84,8 @@ def main(lang, basedir, level, wdir, results):
     worldcat_lang = {"fra": "fre", "eng":"eng", "ita":"ita", "deu":"ger", "por":"por", "spa":"spa", "srp":"srp", "gre":"gre", "ukr":"ukr","slv":"slv", "rom":"rum", "nor":"nor", "cze":"cze", "lit":"lit", "pol":"pol", "frch":"fre", "gsw": "ger"} # dictionary contains ELTeC-language abbreviations as keys, worldcat-language codes as values 
     hit_lang = {"fra":"French", "eng":"English", "ita":"Italian" , "deu":"German", "por":"Portuguese", "spa":"Spanish", "srp":"Serbian", "gre":"Greek, Modern[1453-]", "ukr":"Ukranian", "slv":"Slovenian", "rom":"Romanian", "nor":"Norwegian", "cze":"Czech", "lit":"Lithuanian", "pol":"Polish", "frch": "French", "gsw":"German"} # dictionary contains ELTeC-language abbreviations as keys, worldcat-language category as values
     
-    write_file = join(wdir, "html")
-    htmlpages = join(wdir, "html/*.html")
+    #write_file = join(wdir, "html")
+    #htmlpages = join(wdir, "html/*.html")
     
     lang, d = get_lang(lang, d)
     d, xml_folder = get_xml_folder(d, basedir, level)
@@ -91,8 +93,9 @@ def main(lang, basedir, level, wdir, results):
     d = match_langs(lang, d, worldcat_lang, "lang_worldcat")
     d = match_langs(lang, d, hit_lang, "lang_hit")
     d = get_wdir_file(d, wdir)
-    new_write_file, d = get_write_file(d, write_file)
-    html_folder, d = get_html_file(d, htmlpages)
+    new_write_file, d = get_write_file(d, wdir) #, write_file
+    html_folder, d = get_html_file(d, wdir)
+    #html_folder, d = get_html_file(d, htmlpages)
     d = get_results_file(d, results)
     print(d)
     return d
